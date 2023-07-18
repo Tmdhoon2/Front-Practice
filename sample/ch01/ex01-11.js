50c5163b4e8c3f909617eddec2b459a36d7c5e94
정승훈 <hks026naver@gmail.com>
2023-07-18 10:21:17 +0900

    feat :: 유사배열

--- sample/ch01/ex01-11.js
+++ sample/ch01/ex01-11.js
@@ -10,7 +10,7 @@ add()
 
 // 전달받은 숫자와 100의 합계를 출력한다.
 function add100(n1) {
-
+    console.log(n1 + 100)
 }
 
 add100(10)
@@ -19,7 +19,7 @@ add100(30)
 
 // 전달받은 두 수의 합계를 출력한다.
 function sum(n1, n2) {
-
+    console.log(n1 + n2)
 }
 
 sum(40, 50)
@@ -27,11 +27,9 @@ sum(60, 70)
 
 // 전달받은 두 수의 합계를 반환한다.
 function sum2(n1, n2) {
-
+    return n1 + n2
 }
 
 var returnVal = sum2(70, 80)
 console.log(returnVal)
 console.log(sum2(80, 90))
-
-
--- sample/ch01/ex01-12.js
+++ sample/ch01/ex01-12.js
@@ -1,6 +1,8 @@
 // 지정한 배열의 모든 요소를 출력한다.
-function printArr(arr){
-	
+function printArr(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        console.log(i, arr[i])
+    }
 }
 
 var colorArr = ['orange', 'yellow', 'green'];
@@ -14,18 +16,31 @@ printArr(colorArr);
 //  - length 속성 추가
 //  - 0부터 시작해서 1씩 증가하는 속성 추가
 var arr2 = {
-  push: function(elem){
-    // 마지막 인덱스에 지정한 elem를 추가한다.
+    0: 'orange',
+    1: 'yellow',
+    2: 'green',
+    push: function (elem) {
+        // 마지막 인덱스에 지정한 elem를 추가한다.
+        arr2[length] = elem
+        arr2.length++
+    },
+    pop: function () {
+        // 마지막 요소 추출후 반환(ex03-17.js 참조)
+        var last = arr2[arr2.length-1]
+        arr2.length--
 
-  },
-  pop: function(){
-    // 마지막 요소 추출후 반환(ex03-17.js 참조)
-
-  },
-  shift: function(){
-    // 첫번째 요소 추출후 반환(ex03-17.js 참조)
-
-  }
+        return last
+    },
+    shift: function () {
+        // 첫번째 요소 추출후 반환(ex03-17.js 참조)
+        var first = arr2[0]
+        for(var i = 0; i<arr2.length-1; i++){
+            arrr2[i] = arr2[i+1]
+        }
+        arr2.length--
+        delete arr2[arr2.length]
+        return first
+    }
 };
 arr2.push('black');
 arr2.push('white');
