20b8d6770364f07b9fe2f03ddbca4ae8c02b83fe
정승훈 <hks026naver@gmail.com>
2023-07-18 16:52:27 +0900

    feat :: onclick event

--- hello.js
+++ hello.js
@@ -1,6 +1,6 @@
 window.onload = function () {
   var btn = document.querySelector('button');
-  btn.onclick = function () {
-    console.log('버튼 클릭')
+  btn.onclick = function (event) {
+    console.log('버튼 클릭', event.clientX)
   };
 };
