df6199b1c58eee8c6e04776bcb7a355c98859cae
정승훈 <hks026naver@gmail.com>
2023-07-18 14:10:54 +0900

    feat :: 함수 선언 방식

--- sample/ch02/ex02-04.js
+++ sample/ch02/ex02-04.js
@@ -1,7 +1,12 @@
 // 동적으로 생성된 프로퍼티를 가질 수 있다.
 function add(x, y){
   var result = x + y;
-  return result;
+  return add.result
 }
 
-console.log(add(10, 20));
+console.log(add(10, 20), add(10, 20));
+
+var result = add(30, 40)
+console.log(result, result)
+
+add(50, 60)
--- sample/ch02/ex02-07.js
+++ sample/ch02/ex02-07.js
@@ -1,5 +1,5 @@
 // 표현식 방식의 함수 선언(기명함수)
-
+var add = new Function("x", "y", "return x + y")
 
 console.log(add(10, 20));
 console.log(sum(10, 20));
