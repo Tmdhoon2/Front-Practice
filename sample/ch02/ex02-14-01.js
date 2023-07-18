07df5936a4b557a7181dc7c6d4b109eeab8628ba
정승훈 <hks026naver@gmail.com>
2023-07-18 16:53:02 +0900

    feat :: this

--- sample/ch02/ex02-14-01.js
+++ sample/ch02/ex02-14-01.js
@@ -1,1 +1,22 @@
 // ex02-14.js 복사
+var count = 0;
+var myObj = {
+	count: 0,
+	visit: function(){
+		// 방문자를 한명 증가시킨다.
+        this.count++
+        var that = this
+        var visit2 = function(){
+            that.count++
+        }
+		visit2()
+	},
+    cash: function(){
+        // 자산을 증가시킴
+        this.money += 10
+    }
+};
+
+myObj.visit();
+myObj.visit();
+console.log('합계', myObj.count);
